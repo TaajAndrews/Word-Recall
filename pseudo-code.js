5daf44f74a614676e1d376e763974f3ff8a28f23
Taaj Andrews <taajandrews@gmail.com>
2023-05-07 02:33:20 -0400

    Complete Word Bank for MVP

--- pseudo-code.js
+++ pseudo-code.js
@@ -4,7 +4,7 @@
 // Players can select word buttons on the screen
 // After the player selects their word, the next word and word bank replaces the previous word and the input field turns gold to indicate the correct word was used
 // Players can only use a letter once, so there needs to be guards in place against that
-// For MVP, there will not be a back button, yopu will need to input in the correct order
+// For MVP, there will not be a back button, you will need to input in the correct order
 // track the words that were used in a temporary variable so when played, they will not be used until after the round
 // After each round we can reset the used words so they can be included in the word bank again
 // Reset all variables and begin again
@@ -13,7 +13,7 @@
 // ICE BOX: Once players input a word, they will have a back (erase) button in case they selected the wrong letter
 // ================== 1ST HTML PAGE ========================= //
 // Main game board that looks like wireframe, with a play button
-// CSS Media query: reszpondsive design adjusts for mobile; button siz changes
+// CSS Media query: responsive design adjusts for mobile; button size changes
 // ICE BOX: Main game page consists of the game with the board and everything behind the play button blurred out
 
 // ================== 2nd HTML PAGE ========================= //
--- script.js
+++ script.js
@@ -1,9 +1,84 @@
 /*----- constants -----*/
-
+const wordBank = [
+  {
+    word: "critique",
+    defintion: "to examine in an analytical manner",
+    letters: ["q", "c", "q", "e", "t", "r", "i", "u", "g", "i"],
+    points: 40,
+  },
+  {
+    word: "uptight",
+    definition: "anxious and unable to relax",
+    letters: ["p", "g", "h", "t", "f", "i", "e", "t", "u", "i"],
+    points: 35,
+  },
+  {
+    word: "blatant",
+    definition: "obvious or conspicuous",
+    letters: ["t", "b", "n", "t", "a", "l", "n", "w", "m", "a"],
+    points: 35,
+  },
+  {
+    word: "fabulous",
+    definition: "amazingly good",
+    letters: ["a", "l", "o", "o", "u", "s", "u", "f", "b", "l"],
+    points: 40,
+  },
+  {
+    word: "disburse",
+    definition: "to pay out",
+    letters: ["b", "i", "l", "s", "s", "u", "r", "e", "s", "d"],
+    points: 40,
+  },
+  {
+    word: "ascertain",
+    definition: "to find out",
+    letters: ["c", "a", "r", "n", "e", "r", "i", "s", "t", "a"],
+    points: 45,
+  },
+  {
+    word: "ascribe",
+    definition: "to attribute an outcome to something",
+    letters: ["e", "e", "i", "s", "b", "c", "r", "d", "a", "l"],
+    points: 35,
+  },
+  {
+    word: "concede",
+    definition: "to admit to",
+    letters: ["l", "c", "x", "e", "n", "e", "c", "e", "o", "d"],
+    points: 35,
+  },
+  {
+    word: "expansive",
+    definition: "covering a wide area",
+    letters: ["s", "n", "i", "a", "a", "u", "s", "v", "e", "k"],
+    points: 45,
+  },
+  {
+    word: "disclose",
+    definition: "to provide information to someone",
+    letters: ["o", "y", "c", "i", "s", "d", "l", "e", "s", "e"],
+    points: 40,
+  },
+  {
+    word: "squeeze",
+    definition: "to press tightly",
+    letters: ["u", "u", "e", "t", "s", "e", "h", "q", "z", "e"],
+    points: 35,
+  },
+  {
+    word: "illegible",
+    definition: "unable to read",
+    letters: ["x", "b", "l", "l", "e", "l", "e", "g", "i", "i"],
+    points: 45,
+  },
+]
 /*----- state variables -----*/
+let score // Tracks current score
+let letterBank //
 
 /*----- cached elements  -----*/
 
-/*----- event listeners -----*/
-
 /*----- functions -----*/
+
+/*----- event listeners -----*/
